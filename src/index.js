335e9dc98261a14c9e8f0752c2766630752d3985
Kevin7744 <arapbiisubmisions@gmail.com>
2023-11-26 21:57:56 +0300

    REndering

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Rick and morty');
+import  ReactDOM  from 'react-dom/client';
+
+function App() {
+    return <h1>Rick and Morty</h1>
+}
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<app/>);
